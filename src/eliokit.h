10f8ce0c8781b5c5d7bbd11f5fe43f98e0e1b0e6
Maestry <122365694+maestry@users.noreply.github.com>
2023-02-26 13:17:14 +0100

    Update eliokit.h
    Correzione warning home assistant

--- src/eliokit.h
+++ src/eliokit.h
@@ -5,7 +5,7 @@ extern "C" {
   #include "drivers/i2c/i2cPeripheral.h"
   #include "drivers/sdCard/sdCard.h"
   #include "drivers/accelGyro/accelGyro.h"
-  #include "wireless_charger.h";
+  #include "wireless_charger.h"
   #include "drivers/spi/spiPeripheral.h"
   #include "driver/gpio.h"
   #include "drivers/touchButtonI2C/touchButton.h"
